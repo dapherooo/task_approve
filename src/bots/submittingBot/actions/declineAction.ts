9a21d940097da52d031e4b9465735d01536788b2
andreid-code <andreid.notion@gmail.com>
2026-06-23 15:40:09 +0700

    Update declineAction.ts

--- src/bots/submittingBot/actions/declineAction.ts
+++ src/bots/submittingBot/actions/declineAction.ts
@@ -64,7 +64,7 @@ export const declineAction = async (ctx: BotContext) => {
       `*Tanggal Submit:* ${safeSubmittedDate}\n\n` +
       `*Project:* ${safeProject}\n` +
       `*Assignee:* ${safeAssigneeName}\n\n` +
-      `*➡️ Tuliskan alasan penolakan dengan membalas pesan ini.*`,
+      `*➡️ Tuliskan alasan penolakan dengan membalas pesan ini*`,
       { parse_mode: 'MarkdownV2' }
     );
 
